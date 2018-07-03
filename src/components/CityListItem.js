2fa13605f35120a101e54756833cb7ad10f427a8
Ivana Mrsic <ivana.mrsic@fer.hr>
2018-07-03 17:10:47 +0200

    style changes

--- src/components/CityListItem.js
+++ src/components/CityListItem.js
@@ -59,6 +59,9 @@ const style = StyleSheet.create({
   citySection: {
     flex: 1,
     flexDirection: 'row',
+    borderColor: '#495972',
+    borderWidth: 0.5,
+    padding: 10,
   },
   cityName: {
     fontWeight: 'bold',
@@ -70,7 +73,6 @@ const style = StyleSheet.create({
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-evenly',
-    color: 'white',
   },
   infoText: {
     color: 'white',
