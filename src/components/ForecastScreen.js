0242d590596a3a8e86099a76a1544865b451528d
Ivana Mrsic <ivana.mrsic@fer.hr>
2018-07-10 15:27:22 +0200

    removed unnecessary import

--- src/components/ForecastScreen.js
+++ src/components/ForecastScreen.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { View, FlatList, Text, StyleSheet } from 'react-native';
-import { autobind } from 'core-decorators';
 import PropTypes from 'prop-types';
 import ForecastListItem from './ForecastListItem';
 import * as weatherService from '../services/weather';
